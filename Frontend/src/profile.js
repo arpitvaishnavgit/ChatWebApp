e389b4df600f58904c52c4b3526de360a2e83cf6
Anand dudi <83879799+Ananddudi@users.noreply.github.com>
2022-05-02 23:22:52 +0530

    Update profile.js

--- Frontend/src/profile.js
+++ Frontend/src/profile.js
@@ -3,22 +3,24 @@ import bluebackimage from './statics/forArrow2.gif';
 import firework from './statics/fireworks.gif';
 import {useState,useEffect} from 'react';
 import {RiHomeSmileFill} from 'react-icons/ri';
-import {useHistory} from 'react-router-dom';
-
-
-
 
 export const MyProfile = () => {
-    const [showcontent,setShowcontent] = useState("");
-    const history = useHistory();
-    // document.document.body.style.backgroundImage = `url(${MainImage})`;
-    // document.body.style.backgroundRepeat = "no-repeat";
-    // document.body.style.backgroundSize = "cover";
-    useEffect(() => {
-        setTimeout(() => {
-            setShowcontent(() => {
-                return (
-                <div className="textProfile">
+    const [showcontent,setShowcontent] = useState(false);
+    
+    setTimeout(() => {
+        setShowContent(true)
+        },8000);
+    
+    return (
+        <section className="profilePage">
+            <img src={backimage} className="backimage" alt="cool"/>
+            <div className="redArrow"></div>
+            <img src={bluebackimage} className="bluebackimage"  alt=" "/>
+            <div className="blueArrow"></div>
+            <img src={firework} className="fireworks" alt=" "/>
+            <div className="profileText">Anand dudi</div>
+            {showcontent ?                 
+                <article className="textProfile">
                 <h1 className="headertextProfile">Thanks For Visiting My App! Really Appreciate </h1><h1 className="emoji">&#128522;</h1>
                        <h3 className="frontandback">FrontEnd and Backend :</h3><p className="myname">Anand dudi</p><br/>
                            <div className="detailclass"><h3 className="detailText">Some-Details:</h3><p className="detail">This is my first website.I use javascript's framework <font color="lightcoral">REACT</font> as Frontend and python's framework <font color="lightcoral">DJANGO</font> as backend.<br/>
@@ -30,22 +32,8 @@ export const MyProfile = () => {
                                         websites with good architecture.<RiHomeSmileFill className="smilehome"/>
                             </p></div>
                 <div onClick={()=>setTimeout(()=>{history.push("/")},230)} className="profileBackButton"><span>Back</span></div>
-                </div>
-                );
-            })
-        },8000);
-        // eslint-disable-next-line react-hooks/exhaustive-deps
-    }, [history])
-    return (
-        <section className="profilePage">
-            <img src={backimage} className="backimage" alt="cool"/>
-            <div className="redArrow"></div>
-            <img src={bluebackimage} className="bluebackimage"  alt=" "/>
-            <div className="blueArrow"></div>
-            <img src={firework} className="fireworks" alt=" "/>
-            <div className="profileText">Anand dudi</div>
-            {showcontent}
-        </section>
-
+                </article>
+            }
+       </section>
         );
 }
